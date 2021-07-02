ac324ba0645258eb07bd7dc838ebb1e824962205
Sean Warlick <warlick.sean@gmail.com>
2021-07-02 14:55:23 -0700

    Created and tested method for mapping definition to column names.

--- datadoc/data_documenter.py
+++ datadoc/data_documenter.py
@@ -1,29 +1,32 @@
 """Main module."""
 import pandas as pd
 
+
 class DataDocumenter():
     """Document Class"""
 
     def __init__(self, data_frame: pd.DataFrame) -> None:
         self.data_frame = data_frame
-        pass
 
-    def variable_definitions():
-        pass
+    def variable_definitions(self, definition_mapping: dict):
+        """Assigns a definition to each variable in the data frame"""
+        self._validate_mappers(definition_mapping)
+        final_column_names = {'index': 'variable_name', 0: 'definition'}
+        self.var_definitions = self._mapping_to_df(definition_mapping,
+                                                   final_column_names)
 
     def variable_names(self, name_mapping):
-        """Assignes a meaningful name to the variables in the data frame."""
+        """Assigns a meaningful name to the variables in the data frame."""
+        # Check hat all columns in the mapping are in the data.
+        self._validate_mappers(name_mapping)
+
         # This maping gives better column names at the end of the process.
-        final_column_names = {'index': 'variable_name', 0:'variable'}
+        final_column_names = {'index': 'variable_name', 0: 'variable'}
 
         # Take the name mapping and create a data frame with a colum for the
         # name of the variable in the raw data and a column for the human
         # readable name.
-        self.var_names = (pd.Series(name_mapping)
-                          .to_frame()
-                          .reset_index()
-                          .rename(columns=final_column_names)
-                         )
+        self.var_names = self._mapping_to_df(name_mapping, final_column_names)
 
     def value_labels():
         pass
@@ -36,11 +39,43 @@ class DataDocumenter():
         pass
 
     def _validate_mappers(self, mapper):
+        """Checks that all the keys in mapper dict are in the data frame.
+        This is an internal method used to prevent errors when joining the data.
+        Nothing is returned if all the keys are present in the data frame. It
+        throws an assertion error if one or more of keys are not present in the
+        data frame.
+
+        It does not return an error if columns present in the data frame are not
+        keys in the dictionary.
+
+        Args:
+          mapper: a dictionary where the keys are expected to be columns in the
+            data frame.
+
+        Returns: None
+
+        Raises:
+          AssertionError: One or more of the keys provided in the mapping are
+            not present in the data frame.
+
+        """
         column_names = self.data_frame.columns
         bad_key_error = """One or more keys provided in the mapping are
             not present in the data frame"""
 
-
         present_in_df = [i in column_names for i in mapper.keys()]
 
         assert all(present_in_df), bad_key_error
+
+    def _mapping_to_df(self, mapper, final_column_names):
+        """Converts the mapping dictionary to a data frame
+        An internal function used whenever a dictionary is used to map metadata
+        to the columns of the data frame.
+
+
+        """
+        return (pd.Series(mapper)
+                .to_frame()
+                .reset_index()
+                .rename(columns=final_column_names)
+                )
--- tests/test_data_documenter.py
+++ tests/test_data_documenter.py
@@ -13,7 +13,7 @@ from datadoc import DataDocumenter
 def input_data_frame():
     data = {'col1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             'col2': ['M', 'F', 'M', 'F', 'M', 'M', 'M', None, 'F', 'F'],
-            'col3':[2.2, 3.45, 1.2, 1.1, 5.0, 4.44, 56.6, 2.4, 5, 33.333]}
+            'col3': [2.2, 3.45, 1.2, 1.1, 5.0, 4.44, 56.6, 2.4, 5, 33.333]}
     return pd.DataFrame(data)
 
 
@@ -23,10 +23,24 @@ def var_name_expected():
     return pd.DataFrame({'variable_name': ['col1', 'col2', 'col3'],
                          'variable': ['Age in years', 'Gender', 'Measure']})
 
+
+@pytest.fixture
+def var_definition_expected():
+    """Creates expected output for test_variable_names"""
+    string1 = 'The age of the participant when they joined'
+    string2 = """The participants stated gender.
+    The participant may decline to provide their gender."""
+    string3 = "The distance the participant traveled."
+
+    return pd.DataFrame({'variable_name': ['col1', 'col2', 'col3'],
+                         'definition': [string1, string2, string3]})
+
+
 def test_summary_statistics(input_data_frame):
     """Tests that the function returns summary statistics"""
     x = DataDocumenter(input_data_frame)
     x.summary_statistics()
+
 
 def test_variable_names(input_data_frame, var_name_expected):
     """Tests that variable names are correctly created"""
@@ -36,11 +50,27 @@ def test_variable_names(input_data_frame, var_name_expected):
     var_name_mapping = {'col1': 'Age in years',
                         'col2': 'Gender',
                         'col3': 'Measure'
-                       }
+                        }
 
     x.variable_names(var_name_mapping)
 
     pd.testing.assert_frame_equal(var_name_expected, x.var_names)
+
+
+def test_variable_definition(input_data_frame, var_definition_expected):
+    """Tests that variable names are correctly created"""
+
+    x = DataDocumenter(input_data_frame)
+    def_string2 = """The participants stated gender.
+    The participant may decline to provide their gender."""
+    var_def_mapping = {'col1': 'The age of the participant when they joined',
+                       'col2': def_string2,
+                       'col3': 'The distance the participant traveled.'
+                       }
+
+    x.variable_definitions(var_def_mapping)
+
+    pd.testing.assert_frame_equal(var_definition_expected, x.var_definitions)
 
 
 def test_no_mapping_error(input_data_frame):
@@ -48,17 +78,18 @@ def test_no_mapping_error(input_data_frame):
     var_name_mapping = {'col1': 'Age in years',
                         'col2': 'Gender',
                         'col3': 'Measure'
-                       }
+                        }
     x = DataDocumenter(input_data_frame)
 
     x._validate_mappers(var_name_mapping)
 
+
 def test_mapping_error(input_data_frame):
-    """Tests no error is returned when all keys in the mapper are columns"""
+    """Tests error is returned when a key in the mapper is not a column"""
     var_name_mapping = {'col1': 'Age in years',
                         'col2': 'Gender',
                         'col100': 'Measure'
-                       }
+                        }
 
     x = DataDocumenter(input_data_frame)
     with pytest.raises(AssertionError):
